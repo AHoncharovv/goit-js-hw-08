aaf97444bce2af338dc986e0c88dd1b46edc3e16
Anton Honcharov <antonhoncharovv@gmail.com>
2022-02-17 23:09:41 +0300

    upd video

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const makeGalleryItemMarkup = (item) => {
+    return `<li>
+            <div class="gallery">
+                <a class="gallery__item" href="${item.original}">
+                    <img class="gallery__image" src="${item.preview}" alt="${item.description}"/>
+                </a>
+            </div>    
+        </li>`   
+        ;
+};
+
+const makeGalleryMarkup = galleryItems.map(makeGalleryItemMarkup).join('');
+
+const galleryElements = document.querySelector('.gallery');
+
+galleryElements.insertAdjacentHTML('beforeend', makeGalleryMarkup);
+
+
+let lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+require('@vimeo/player');
+require('lodash.throttle');
 
+// codding
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+    
+let time = 0;
+
+player.on('play', function (timeupdate) {
+    time = timeupdate.seconds;
+        console.log(time);
+    });
+
+    
