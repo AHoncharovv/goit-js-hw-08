70970e414a30c853f7d9a7c6bd1bf65ea29cc3a3
Anton Honcharov <antonhoncharovv@gmail.com>
2022-02-19 12:32:25 +0300

    add throttle

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,3 +1,5 @@
+const throttle = require('lodash.throttle');
+
 require('@vimeo/player');
 require('lodash.throttle');
 
@@ -5,12 +7,33 @@ require('lodash.throttle');
 
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
-    
-let time = 0;
 
-player.on('play', function (timeupdate) {
-    time = timeupdate.seconds;
-        console.log(time);
-    });
 
+
+
+player.on('timeupdate', throttle(data => {
+    localStorage.setItem("videoplayer-current-time", data.seconds.toString());
+}, 1000));
+
+const onPlay = () => {
+    localStorage.getItem("videoplayer-current-time");
+};
+
+player.on('play', onPlay);
+//////////////////////
     
+// let timeHolder = 0;
+
+// player.on('play', function (timeupdate) {
+//     timeHolder = timeupdate.seconds;
+//     localStorage.setItem("videoplayer-current-time",  timeHolder);
+// });
+
+////////////////////////////////////////////////////
+    
+// player.on('timeupdate', () => {
+//     console.log(localStorage.getItem("videoplayer-current-time").seconds.toString())
+// });
+
+
+
