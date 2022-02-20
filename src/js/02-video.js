019c29696428c9223fca34c660eb28ea06b127c9
Anton Honcharov <antonhoncharovv@gmail.com>
2022-02-20 21:43:30 +0300

    add video and feedback

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -8,32 +8,16 @@ require('lodash.throttle');
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
-
-
-
 player.on('timeupdate', throttle(data => {
     localStorage.setItem("videoplayer-current-time", data.seconds.toString());
 }, 1000));
+
 
 const onPlay = () => {
     localStorage.getItem("videoplayer-current-time");
 };
 
 player.on('play', onPlay);
-//////////////////////
-    
-// let timeHolder = 0;
-
-// player.on('play', function (timeupdate) {
-//     timeHolder = timeupdate.seconds;
-//     localStorage.setItem("videoplayer-current-time",  timeHolder);
-// });
-
-////////////////////////////////////////////////////
-    
-// player.on('timeupdate', () => {
-//     console.log(localStorage.getItem("videoplayer-current-time").seconds.toString())
-// });
 
 
-
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time"))
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,50 @@
+const throttle = require('lodash.throttle');
+
+const form = document.querySelector("form");
+ 
+const userMessage = {
+    email: "",
+    message: "",
+};
+
+form.addEventListener("submit", onFormSubmit);
+form.addEventListener("input", throttle(onFormInput, 500));
+
+updateUserMessage();
+
+function updateUserMessage() {
+    const savedMessage = localStorage.getItem('feedback-form-state');
+    if (savedMessage) {
+        const { email, message } = JSON.parse(savedMessage);
+        form.email.value = email;
+        form.message.value = message;
+        userMessage.email = email;
+        userMessage.message = message;
+    }
+}
+
+function onFormSubmit(event) { 
+    event.preventDefault();
+    const savedMessage = localStorage.getItem('feedback-form-state');
+    const parsedMessage = JSON.parse(savedMessage);
+
+    console.log(parsedMessage);
+
+    event.currentTarget.reset();
+    localStorage.removeItem("feedback-form-state");
+}
+
+
+function onFormInput(event) {
+   
+    if (event.target.nodeName === 'INPUT') {
+        userMessage.email = event.target.value;
+    } else {
+        userMessage.message = event.target.value;
+    };
+    localStorage.setItem("feedback-form-state", JSON.stringify(userMessage));
+}
+
+
+
 
